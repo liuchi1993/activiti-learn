21750d6813695fe76d8e98b7a2cfd301ad207381
liuchi1993 <605227652@qq.com>
2019-11-15 23:49:13 +0800

    init project activiti demo

---
+++ activiti/src/test/java/cn/liuc/activitidemo/ActivitiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liuc.activitidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
